036fcc036e156abb7c40e0e81a064c40b93228b3
Donald Besong <donald.besong.principles@gmail.com>
2023-03-22 17:47:06 +0000

    changed app to application

--- application.py
+++ application.py
@@ -2,4 +2,5 @@ from app import create_app
 application = create_app('default')
 #windows cmd ren gitignore.txt .gitignore #
 if __name__ == "__main__":
-    application.run(host='0.0.0.0', port='8080')
+    application.run(host='0.0.0.0')
+    #application.run(host='0.0.0.0', port='8080')
